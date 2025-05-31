d8135738e43aa47c980718a59360171a517d1389
CodeKartiks <136325206+CodeKartiks@users.noreply.github.com>
2025-06-01 02:38:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-// server.js
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Render-deployed Node.js app!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
